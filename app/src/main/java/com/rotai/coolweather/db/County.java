9e81295105af2618777699cddd0f3b3b9a121f0c
RONGTAI\rt005656 <rt006103@rotai.com>
2018-10-09 13:11:42 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/rotai/coolweather/db/County.java
@@ -0,0 +1,48 @@
+package com.rotai.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * id:实体类通用字段
+ * countyName：县的名字
+ * weatherId：县对应的天气id
+ * cityId:当前县所属市的id
+ */
+public class County extends DataSupport {
+    private int id;
+    private String countyName;
+    private String weatherId;
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
